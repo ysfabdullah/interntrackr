d79390553943cc29de602ccc5d6c183df31623ea
Abdullah Yousaf <abdullahyousaf21@gmail.com>
2025-12-27 19:35:09 -0700

    addded themes and colors for page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,8 @@ import toast from 'react-hot-toast';
 
 const App = () => {
   return (
-    <div>
-
+    <div data-theme="cupcake">
+      <button className="btn">Button</button>
       <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/create" element={<CreatePage />} />
