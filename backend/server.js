0bc2d611b1d3571c1f19505248ea0f5dee78264c
Abdullah Yousaf <abdullahyousaf21@gmail.com>
2025-12-27 22:39:38 -0700

    added cors for backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 import express from "express"
 import internRoutes from "./routes/internRoutes.js";
 import { connect } from "mongoose";
@@ -6,11 +7,20 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// middleware
 app.use(express.json());
 app.use(rateLimiter);
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    }
+
+))
 
 
 app.use("/api/internships", internRoutes);
