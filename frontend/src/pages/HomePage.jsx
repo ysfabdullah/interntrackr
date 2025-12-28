32ceaac4ff1ab6b2b4a6ea9f3bb1a2524ac39dec
Abdullah Yousaf <abdullahyousaf21@gmail.com>
2025-12-27 22:11:33 -0700

    Ran into a bug, will fix later. Finished some progress on setting homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../componets/Navbar';
 import RateLimitedUI from '../componets/RateLimitedUI';
 import { useState } from 'react';
 
+
 const HomePage = () => {
   const [israteLimited, setIsRateLimited] = useState(true);
+  const [internships, setInternships] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchInternships = async () => {
+      try {
+        const response = await axious.get('https://interntrackr-backend.onrender.com/api/internships');
+      const data = await response.json();
+      console.log(data);
+      
+      } catch (error) {
+        console.error('Error fetching internships:', error)
+      }
+  }
+
+  fetchInternships();
+  }, [])
+
 
   return (
     <div classname="min-h-screen">
